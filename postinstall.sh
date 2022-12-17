68960fb6a7a032f5f4081d1ea3b3a1e49a7c2bde
aamirali51 <62554127+aamirali51@users.noreply.github.com>
2022-12-17 12:53:21 +0500

    update

--- postinstall.sh
+++ postinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-mkdir -p "${DESTDIR}/${MESON_INSTALL_PREFIX}/share/pakoswelcome/"
-cp -r "${MESON_SOURCE_ROOT}/src/scripts" "${DESTDIR}/${MESON_INSTALL_PREFIX}/share/pakoswelcome/"
-cp -r "${MESON_SOURCE_ROOT}/data" "${DESTDIR}/${MESON_INSTALL_PREFIX}/share/pakoswelcome/"
-cp -r "${MESON_SOURCE_ROOT}/ui" "${DESTDIR}/${MESON_INSTALL_PREFIX}/share/pakoswelcome/"
+mkdir -p "${DESTDIR}/${MESON_INSTALL_PREFIX}/share/pakos-welcome/"
+cp -r "${MESON_SOURCE_ROOT}/src/scripts" "${DESTDIR}/${MESON_INSTALL_PREFIX}/share/pakos-welcome/"
+cp -r "${MESON_SOURCE_ROOT}/data" "${DESTDIR}/${MESON_INSTALL_PREFIX}/share/pakos-welcome/"
+cp -r "${MESON_SOURCE_ROOT}/ui" "${DESTDIR}/${MESON_INSTALL_PREFIX}/share/pakos-welcome/"
 
 cd "${MESON_SOURCE_ROOT}/po"
 for lang in $(ls *.po); do
